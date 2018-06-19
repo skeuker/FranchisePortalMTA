9536bfec166d9aa455b8e9e8028a4e841f6e327c
S0004142753 <skeuker@pnp.co.za>
2018-06-19 12:16:44 +0000

    Prepare for Grunt

--- order_ui/Gruntfile.js
+++ order_ui/Gruntfile.js
@@ -1,7 +1,17 @@
 module.exports = function(grunt) {
 	"use strict";
-	grunt.loadNpmTasks("@sap/grunt-sapui5module-bestpractice-build");
+	
+	//load grunt SAP UI5 best practice build
+	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
+	
+	//write to build log
+    grunt.log.writeln("\n### grunt.config() ###\n" + JSON.stringify(grunt.config(), null, 2));
+    
+    //do the build
 	grunt.registerTask("default", [
-		"lint"
+        "lint",
+        "clean",
+        "build"
 	]);
+	
 };
