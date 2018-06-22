21d80227d2d566e702d833c34aae4e8ee6c03d07
S0004142753 <skeuker@pnp.co.za>
2018-06-22 09:14:20 +0000

    Intermittent commit

--- admin_ui/Gruntfile.js
+++ admin_ui/Gruntfile.js
@@ -2,16 +2,14 @@ module.exports = function(grunt) {
 	"use strict";
 	
 	//load grunt SAP UI5 best practice build
-	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
+	grunt.loadNpmTasks("@sap/grunt-sapui5module-bestpractice-build");
 	
 	//write to build log
     grunt.log.writeln("\n### grunt.config() ###\n" + JSON.stringify(grunt.config(), null, 2));
     
-    //do the build    
+    //do the build
 	grunt.registerTask("default", [
-		"lint",
-		"clean",
-    	"build"
+        "lint"
 	]);
 	
 };
